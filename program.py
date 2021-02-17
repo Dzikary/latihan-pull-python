9aa52d459d84554755482559e771f4ce5365c5f8
M Dzaky Ghifari Madjid <69739492+Dzikary@users.noreply.github.com>
2021-02-17 21:38:33 +0700

    Update program.py

--- program.py
+++ program.py
@@ -1,15 +1,21 @@
 class BioData:
-  def __init__(self, nama, kelas):
+  def __init__(self, nama, kelas, umur, agama):
     self.nama = nama
     self.kelas = kelas
     self.umur = umur
     self.agama = agama
    
    def printNama(self):
-    print("Nama anda: {self.nama}")
+    print(F"Nama anda: {self.nama}")
     
-   def printKelas():
-    print("Kelas anda: {kelas}")
+   def printKelas(kelas):
+    print(F"Kelas anda: {kelas}")
+
+ def printKelas(umur):
+    print(f"Umur anda: {umur}")
+
+ def printKelas(Agama):
+    print(f"Kelas anda: {agama}")
    
 Ahmad = BioData("Ahmad Borat", "12 IPA 1", "18 Tahun", "Islam")
 Ahmad.printNama()
